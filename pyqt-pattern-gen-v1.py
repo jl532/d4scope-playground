006192acd3d5d1ea10f6ff39b473285e88b4cbdb
Jason L. big rig <JL532@duke.edu>
2022-06-21 15:44:15 -0400

    basic Mainwindow, need to add in textboxes with boilerplate and have people enter in shit, and then update

--- pyqt-pattern-gen-v1.py
+++ pyqt-pattern-gen-v1.py
@@ -8,3 +8,25 @@ Created on Tue Jun 21 14:23:23 2022
 @author: jason
 """
 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QHBoxLayout, QVBoxLayout, QWidget
+
+import sys
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.centralwidget = QWidget()
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.plotting_widget = QWidget()
+        self.verticalLayout.addWidget(self.plotting_widget)
+        
+        self.button = QPushButton("Push for Window")
+        self.verticalLayout.addWidget(self.button)
+        self.setCentralWidget(self.centralwidget)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
